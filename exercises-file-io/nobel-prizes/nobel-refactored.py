c43525ae23283691c070f583298ed8455fa2cf78
rcmadden <dataengineer007@gmail.com>
2022-03-04 17:37:32 -0800

    debugged for loop and applied syntax to list comprension

--- exercises-file-io/nobel-prizes/nobel-refactored.py
+++ exercises-file-io/nobel-prizes/nobel-refactored.py
@@ -1,29 +1,47 @@
 import json
+from sys import stdout
+from textwrap import indent
 import helper
 
-def load_nobel_prizes(filename='../../data/prize.json'):
+# def load_nobel_prizes(filename='../../data/prize.json'):
+def load_nobel_prizes(filename='data/prize.json'):
     with open(filename) as json_file:
         return json.load(json_file)
-    
+
+
 def main(year, category):
     data = load_nobel_prizes()
     prizes = data['prizes']
 
+    # list comprehension
+    prize_list0 = [prize for prize in prizes if prize['year'] == '2021']
+    json.dump(prize_list0, stdout,indent=2)
+    prize_list1 = []
+
+    # looping list
     for prize in prizes:
-        # print(prize)
-        if 'laureates' not in prize:
-            continue
-        # i = 0
-        if prize['year'] != year:
-            continue
-            # print(prize['year'])
-            # print(prize['laureates'])
-        print(f"{prize['year']} Nobel Prize in {prize['category'].title()}")
-        for laureate in prize['laureates']:
-            firstname = laureate['firstname']
-            surname = laureate.get('surname', '')
-            print(firstname, surname)
-        print('\n')
+        if prize['year'] == '2021':
+            prize_list1.append(prize)
+    json.dump(prize_list1, stdout, indent=2)
+    
+    # json_string = json.dumps(data)
+    # for prize in prizes:
+    #     # print(prize)
+    #     if 'laureates' not in prize:
+    #         continue
+    #     # i = 0
+    #     if prize['year'] != year:
+    #         continue
+    #         # print(prize['year'])
+    #         # print(prize['laureates'])
+    #     print(f"{prize['year']} Nobel Prize in {prize['category'].title()}")
+    #     for laureate in prize['laureates']:
+    #         firstname = laureate['firstname']
+    #         surname = laureate.get('surname', '')
+    #         print(firstname, surname)
+    #         # todo print options
+    #         print()
+    #     print('\n')
             # print(prize['year'], prize['category'])
             # for i in range(len(prize['laureates'])):
             #     if prize.get(['laureates'][i]['surname']) in prize:
